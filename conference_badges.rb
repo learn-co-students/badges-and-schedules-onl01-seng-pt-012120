0bc256cddaeb331f16f5ff6687440d95283ebd4e
fadimata tillman <fadi.tillman@gmail.com>
2020-01-29 17:36:34 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+
+  def badge_maker(name)
+   return"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    badge_maker(name)
+    end
+end
+
+
+def assign_rooms(speackers)
+  speackers.map.with_index do |name,index|
+   "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+
+ def printer(speakers)
+   batch_badge_creator(attendees).map do |person|
+     puts person
+   end
+   assign_rooms(attendees).map do |person|
+     puts person
+   end
+end
