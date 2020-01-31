1c6a51423ca06c9e371f8d8a89a3528579345968
Chris Beyer <beyercorpuz@gmail.com>
2020-01-31 00:53:50 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,16 @@
 # Write your code here.
-def batch_badge_creator(name)
-  name =["Edsger","Ada","Charles","Alan","Grace","Linus","MatzA"]
-  end
-  def    badge_messages 
-  "Hello, My name is {name} +=
-  end
-def assignments("room")
-room=[1,2,3,4,5,6,7]
-Puts " Hello, {name}+ You'll  be assigned to room{ room}+= 
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
-end
+
+#def batch_badge_creator(name)
+ 
+# 3 name =["Edsger","Ada","Charles","Alan","Grace","Linus","MatzA"]
+#  end
+ #5,6,7]
+#Puts " Hello, {name}+ You'll  be assigned to room{ room}+= 
+#end
+#end
 
 #Write badge creator
 #array of names as argument
