c7629bf5949c70c9e0af8f3fb091bc55f28550c5
Alvaro Armas <aarmas92@outlook.com>
2020-02-01 16:15:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,35 +1,29 @@
 # Write your code here.
 
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  badge_messages = []
-  speakers.each do |speaker|
-    message = badge_maker(speaker)
-    badge_messages << message
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
   end
-  badge_messages
 end
 
-def assign_rooms(speakers)
-  room_number = 1
-  room_messages = []
-  speakers.each do |speaker|
-    room_messages << "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
-    room_number += 1
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
-  room_messages
 end
 
-def printer(speakers)
-  badge_messages = batch_badge_creator(speakers)
-  badge_messages.each do |message|
-    puts message
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
   end
-  room_messages = assign_rooms(speakers)
-  room_messages.each do |message|
-    puts message
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
   end
 end
